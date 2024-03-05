3e4c16499b3852277d4485df2a70aa5184a9dd26
Antonio Iaccarino <einginant@gmail.com>
2024-03-06 11:27:14 +1300

    feat: basic query

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,8 +2,19 @@
 
 import Form from "@/src/components/form";
 import Table from "@/src/components/table";
+import { EventLog } from "@/src/types/event-log";
 import { LogsForm } from "@/src/types/form";
+import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
+
+const getLogs = async (form: LogsForm) => {
+  const response = await fetch("http://localhost:3030/events/log");
+
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return await response.json();
+};
 
 export default function Home() {
   const [logForm, setLogForm] = useState<LogsForm>({
@@ -15,13 +26,27 @@ export default function Home() {
     sortOrder: "ASC",
   });
 
+  const query = useQuery<EventLog[]>({
+    placeholderData: [],
+    queryKey: [
+      "logs",
+      logForm.startDate,
+      logForm.endDate,
+      logForm.userId,
+      logForm.eventType,
+      logForm.sortBy,
+      logForm.sortOrder,
+    ],
+    queryFn: () => getLogs(logForm),
+  });
+
   return (
     <main className="flex min-h-screen w-full max-h-screen">
       <div className="flex min-h-screen w-1/3 bg-white">
         <Form updateForm={setLogForm} />
       </div>
       <div className="flex min-h-screen w-2/3 bg-slate-900">
-        <Table data={[]} />
+        <Table data={query.data} />
       </div>
     </main>
   );
